0c747965f83bd6ef37b041f1a35e9adeb1ade7a5
saurabh kumar <saurabhaggarwal2017@gmail.com>
2023-09-07 20:04:47 +0530

    im adding the second.java file for sum of two numbers.

---
+++ Second.java
@@ -0,0 +1,8 @@
+public class Second {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.print("sum is: ");
+        System.out.println(a+b);
+    }
+}
