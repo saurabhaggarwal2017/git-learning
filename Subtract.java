9e8431c0a30a1aa1b00434c37a1b05b657353e72
saurabh kumar <saurabhaggarwal2017@gmail.com>
2023-09-07 20:25:32 +0530

    im adding subtract file

---
+++ Subtract.java
@@ -0,0 +1,9 @@
+public class Subtract {
+    public static void main(String[] args) {
+        int a = 30;
+        int b = 20;
+
+        System.out.print("Subtraction is : ");
+        System.out.println(a - b);
+    }
+}
